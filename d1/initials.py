7928242f84f3a881d5fc240798acb049464a3073
Parrr Geng <parrr.geng@gmail.com>
2023-01-30 10:46:17 +0100

    first commit

---
+++ d1/initials.py
@@ -0,0 +1,10 @@
+#Wei Geng
+#I love spinning!
+
+print("W   W  GGGG")
+print("W   W G    ")
+print("W   W G  GG")
+print("W   W G   G")
+print("W W W G   G")
+print("WW WW G   G")
+print("W   W  GGG ")
